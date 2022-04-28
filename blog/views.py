9198dc2bd7952feec60fdd84605dbe01ca221b10
hello <nurkamila.turathan.kyzy@gmail.com>
2022-04-28 12:29:09 +0600

    molchanov3

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def posts_list(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    n = ['Aika', 'Baika', 'Maika']
+    return render(request, 'blog/index.html', context={'names': n})
